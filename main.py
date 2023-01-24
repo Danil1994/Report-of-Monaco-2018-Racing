10a281e54405fe2599af5adf77611eb912b1c9c9
Danil1994 <danilaleksandrovich36@gmail.com>
2023-01-24 15:25:01 +0300

    add if name

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from src.main.cli import parser, create_list_object, print_driver
 from src.main.main_class import print_ascending, print_descending
 
-cli_command = parser()
-object_list = create_list_object(cli_command.files)
-if cli_command.order == 'asc':
-    print_ascending(object_list)
-if cli_command.order == 'desc':
-    print_descending(object_list)
-if cli_command.driver:
-    print_driver(object_list, cli_command.driver)
-
 if __name__ == '__main__':
-    parser()
+    cli_command = parser()
+    object_list = create_list_object(cli_command.files)
+    if cli_command.order == 'asc':
+        print_ascending(object_list)
+    if cli_command.order == 'desc':
+        print_descending(object_list)
+    if cli_command.driver:
+        print_driver(object_list, cli_command.driver)
